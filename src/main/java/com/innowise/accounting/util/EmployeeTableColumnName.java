7d1dd88d7967f9e12035755dea79fe4a009cb61b
olga-bylnova <olga.bylnova@yandex.by>
2023-03-18 21:23:42 +0300

    add util class for db table column name constants

---
+++ src/main/java/com/innowise/accounting/util/EmployeeTableColumnName.java
@@ -0,0 +1,14 @@
+package com.innowise.accounting.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class EmployeeTableColumnName {
+    public static final String ID = "id";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String BIRTHDAY = "birthday";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String ROLE = "role";
+}
