58e33e64783f9223db3c0940d1a1ffab82862959
olga-bylnova <olga.bylnova@yandex.by>
2023-03-19 17:36:12 +0300

    add util classes for http method names and url paths

---
+++ src/main/java/com/innowise/accounting/util/HttpMethod.java
@@ -0,0 +1,11 @@
+package com.innowise.accounting.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class HttpMethod {
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String PUT = "PUT";
+    public static final String DELETE = "DELETE";
+}
---
+++ src/main/java/com/innowise/accounting/util/UrlPath.java
@@ -0,0 +1,11 @@
+package com.innowise.accounting.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UrlPath {
+    public static final String LOGIN = "/login";
+    public static final String REGISTRATION = "/registration";
+    public static final String LOGOUT = "/logout";
+    public static final String EMPLOYEES = "/employees";
+}
